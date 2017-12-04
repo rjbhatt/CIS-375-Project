eea141cce3e668a53c32a115b739174c3fdf506a
Rakshit Bhatt <rjbhatt@umich.edu>
2017-12-03 19:56:30 -0500

    v1.0
    1st Commit

---
+++ BootstrapSite1/Controllers/OtherController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+
+namespace BootstrapSite1.Controllers
+{
+    
+    
+    public class OtherController : Controller
+    {
+        
+        public ActionResult FullWidth()
+        {
+            return View();
+        }
+        
+        public ActionResult SideBar()
+        {
+            return View();
+        }
+        
+        public ActionResult Faq()
+        {
+            return View();
+        }
+    }
+}
